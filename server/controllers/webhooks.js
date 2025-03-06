ab9cba4e95482bf933fc034b72e3ca5c5e33c8e4
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2025-03-06 13:32:58 +0545

    Error Fixed

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -4,59 +4,53 @@ import User from "../models/User.js";
 //API Controller function to manage CLERK user with Database
 export const clerkWebhooks = async (req, res) => {
     try {
-
         //Create a Sivix Instance with Clerk webhook Secret.
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
         //Veryfing Headers
-        await whook.verify(JSON.stringify(req.body), {
+        await whook.verify(req.body, {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
-        })
+        });
 
-        //Getting DAta from request body
-        const { data, type } = req.body
+        //Getting Data from request body
+        const { data, type } = req.body;
 
         //Switch Case for different events
         switch (type) {
             case 'user.created': {
                 const userData = {
-                    _id:data.id,
-                    email:data.email_address[0].email_address,
-                    name:data.first_name + " " + data.last_name,
+                    _id: data.id,
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
                     image: data.image_url,
                     resume: ''
-                }
-                await User.create(userData)
-                req.json({})
+                };
+                await User.create(userData);
+                res.json({});
                 break;
             }
             case 'user.updated': {
                 const userData = {
-                    email:data.email_address[0].email_address,
-                    name:data.first_name + " " + data.last_name,
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
                     image: data.image_url,
-                }
-                await User.findByIdAndUpdate(data.id, userData)
-                req.json({})
+                };
+                await User.findByIdAndUpdate(data.id, userData);
+                res.json({});
                 break;
-
             }
             case 'user.deleted': {
-                await User.findByIdAndDelete(data.id)
-                req.json({})
+                await User.findByIdAndDelete(data.id);
+                res.json({});
                 break;
-
             }
             default:
                 break;
-
-
         }
-
     } catch (error) {
         console.log(error.message);
-        req.json({success:false, message:'Webhooks Error'})
+        res.json({ success: false, message: 'Webhooks Error' });
     }
-}
+};
--- server/server.js
+++ server/server.js
@@ -22,8 +22,9 @@ app.get('/', (req, res)=> res.send("API Working"))
 app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
-  app.post('/webhooks', clerkWebhooks)
-  
+  app.post('/webhooks', express.raw({ type: 'application/json' }), clerkWebhooks);
+
+
 
 // port
 const PORT = process.env.PORT || 5000
