641481871bfa9405c274398988b554a9639cbd05
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2025-03-06 19:18:42 +0545

    checking error

--- server/config/db.js
+++ server/config/db.js
@@ -3,13 +3,15 @@ import mongoose from "mongoose";
 // Function to Connect MongoDB database
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+        await mongoose.connect(process.env.MONGODB_URI, { dbName: "job-portal" }); 
+
+        
 
         console.log("Database Connected Successfully...");
     } catch (error) {
         console.error("Database Connection Failed:", error.message);
         process.exit(1);
-    }
+    }       
 };
 
 export default connectDB;
