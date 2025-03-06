0ad9cf416c5c50f5c5abc5a78458810b36ee217a
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2025-03-06 13:49:12 +0545

    Db error

--- server/config/db.js
+++ server/config/db.js
@@ -3,14 +3,7 @@ import mongoose from "mongoose";
 // Function to Connect MongoDB database
 const connectDB = async () => {
     try {
-        const uri = process.env.MONGODB_URI;  // Ensure this is a valid URI in your .env file
-        if (!uri) {
-            console.error("MongoDB URI not provided in .env file.");
-            process.exit(1);
-        }
-
-        // Connect to MongoDB without deprecated options
-        await mongoose.connect(uri);
+        await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
 
         console.log("Database Connected Successfully...");
     } catch (error) {
